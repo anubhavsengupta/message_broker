e460b20e92a32642bfb9d8d4e64167e3ff618829
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2025-03-16 20:38:25 -0400

    Setup client socket

--- client.cpp
+++ client.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
-using namespace std;
+#include <arpa/inet.h>   // for htons and htonl
+#include <netinet/in.h>  // for sockaddr_in
+#include <sys/socket.h>  // for socket, bind, listen
+#include <stdlib.h>      // for exit
 
-
-int main() {
-    cout << "Hello worldQ" << endl;
-    return 0;
+// 'die' function is defined to handle errors.
+void die(const char* msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
+int main() {
+    // Create a socket (file descriptor)
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+        die("socket()");
+    }
+
+    // set up  server address structure
+    struct sockaddr_in serverAddress = {};
+    serverAddress.sin_family = AF_INET;         
+    serverAddress.sin_port = htons(1234);            
+    serverAddress.sin_addr.s_addr = htonl(0);           //  wildcardaddress (0.0.0.0)
+
+    // bind socket to the specified address and port
+    int bindResult = bind(socket_fd, (const struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (bindResult != 0) {
+        die("bind()");
+    }
+
+    // Start listening for incoming connections 
+    if (listen(socket_fd, 10) < 0) {
+        die("listen()");
+    }
+    
+    return 0;
+}
