cdb9ab4d4752b6bd822f0d6d1abd787a4d003a64
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2025-04-03 16:52:41 -0400

    Add event loop

--- client.cpp
+++ client.cpp
@@ -1,15 +1,27 @@
 #include <arpa/inet.h>   // using inet_pton, htons
 #include <netinet/in.h>  // using sockaddr_in
-#include <sys/socket.h>  // using socket, connect, send
+#include <sys/socket.h>  // using socket, connect, send, recv
+#include <unistd.h>      // using close, read
 #include <stdlib.h>      // using exit
 #include <stdio.h>
 #include <string.h>      // using memset, strlen
-#include <unistd.h>      // using close
-
+#include <fcntl.h>       // using fcntl, O_NONBLOCK
+#include <poll.h>        // using poll
+#include <vector>
 
 void die(const char* msg) {
     perror(msg);
     exit(EXIT_FAILURE);
+}
+
+// helper to set a fd to non-blocking mode
+static void set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0)
+        die("fcntl(get)");
+    flags |= O_NONBLOCK;
+    if (fcntl(fd, F_SETFL, flags) < 0)
+        die("fcntl(set)");
 }
 
 int main() {
@@ -19,13 +31,13 @@ int main() {
         die("socket()");
     }
 
-    // setup server
+    // setup server address
     struct sockaddr_in serverAddress;
     memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(1234);  
-
-    // set server IP address to just be localhost
+    serverAddress.sin_port = htons(1234);
+    
+    // set server ip address to localhost
     if (inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr) <= 0) {
         die("inet_pton()");
     }
@@ -34,16 +46,61 @@ int main() {
     if (connect(client_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
         die("connect()");
     }
+    
+    // set the client socket to non-blocking mode
+    set_nonblocking(client_fd);
 
-    // send a message to the server.
+    // send a message to the server
     const char* msg = "Hello, server!";
     ssize_t bytes_sent = send(client_fd, msg, strlen(msg), 0);
     if (bytes_sent < 0) {
         die("send()");
     }
-    printf("Message sent: %s\n", msg);
+    printf("message sent: %s\n", msg);
 
-    // close connection 
+    // create a vector of pollfd, we'll only have our client fd here
+    std::vector<struct pollfd> pollfds;
+    struct pollfd pfd;
+    pfd.fd = client_fd;
+    pfd.events = POLLIN;  // we want to know when data is available to read
+    pfd.revents = 0;
+    pollfds.push_back(pfd);
+
+    // event loop to wait for a response from the server
+    while (true) {
+        // wait for readiness (5 second timeout here, adjust as needed)
+        int rv = poll(pollfds.data(), pollfds.size(), 5000);
+        if (rv < 0) {
+            if (errno == EINTR)
+                continue;  // interrupted by a signal, retry
+            die("poll()");
+        }
+        if (rv == 0) {
+            // timeout occurred, no response received
+            printf("no response from server, timing out\n");
+            break;
+        }
+        // check if our client fd is ready to read
+        if (pollfds[0].revents & POLLIN) {
+            char buffer[1024] = {0};
+            ssize_t bytesRead = read(client_fd, buffer, sizeof(buffer) - 1);
+            if (bytesRead < 0) {
+                if (errno == EAGAIN || errno == EWOULDBLOCK)
+                    continue;  // nothing available, try again
+                die("read()");
+            }
+            if (bytesRead == 0) {
+                printf("server closed connection\n");
+                break;
+            }
+            buffer[bytesRead] = '\0';
+            printf("received from server: %s\n", buffer);
+            // assume we exit after receiving the response
+            break;
+        }
+    }
+
+    // close connection
     close(client_fd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -5,34 +5,46 @@
 #include <stdlib.h>      // for exit
 #include <stdio.h>
 #include <string.h>      // for memset, strlen
-
+#include <fcntl.h>       // for fcntl, O_NONBLOCK
+#include <poll.h>        // for poll
+#include <vector>
 
 void die(const char* msg) {
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
+// helper to set a fd to non-blocking mode
+static void set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0)
+        die("fcntl(get)");
+    flags |= O_NONBLOCK;
+    if (fcntl(fd, F_SETFL, flags) < 0)
+        die("fcntl(set)");
+}
+
 int main() {
-    // Create the socket
+    // create the listening socket
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd < 0) {
         die("socket()");
     }
 
-    // reuse port everytime we compile
+    // reuse port so we don't have to wait for the socket to free up
     int opt = 1;
     if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         die("setsockopt()");
     }
 
-    // setup server
+    // setup server address
     struct sockaddr_in serverAddress;
     memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(1234);  // Port 1234
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on all interfaces (0.0.0.0)
+    serverAddress.sin_port = htons(1234);  // port 1234
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // listen on all interfaces
 
-    // bind socket to address and port
+    // bind the socket to the address and port
     if (bind(socket_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0) {
         die("bind()");
     }
@@ -41,46 +53,109 @@ int main() {
     if (listen(socket_fd, 10) < 0) {
         die("listen()");
     }
-    printf("Server listening on port 1234...\n");
+    printf("server listening on port 1234...\n");
 
-    // handle incoming connections
+    // set the listening socket to non-blocking mode
+    set_nonblocking(socket_fd);
+
+    // create a vector of pollfd structures
+    std::vector<struct pollfd> pollfds;
+    // add the listening socket at index 0
+    struct pollfd pfd;
+    pfd.fd = socket_fd;
+    pfd.events = POLLIN; // we want to know when new connections come in
+    pfd.revents = 0;
+    pollfds.push_back(pfd);
+
+    // event loop
     while (true) {
-        struct sockaddr_in clientAddress;
-        socklen_t clientAddrSize = sizeof(clientAddress);
-        int connfd = accept(socket_fd, (struct sockaddr *)&clientAddress, &clientAddrSize);
-        if (connfd < 0) {
-            perror("accept()");
-            continue;  // error, try accepting the next connection
+        // wait for readiness on our fds (blocking call, but only here)
+        int rv = poll(pollfds.data(), pollfds.size(), -1);
+        if (rv < 0) {
+            if (errno == EINTR)
+                continue; // interrupted by a signal, retry
+            die("poll");
         }
-        printf("Accepted a connection.\n");
 
-        // process client requests until error
-        while (true) {
-            char buffer[1024] = {0};
-            ssize_t bytesRead = read(connfd, buffer, sizeof(buffer) - 1);
-            if (bytesRead < 0) {
-                perror("read()");
-                break;
-            }
-            if (bytesRead == 0) {
-                printf("Client disconnected.\n");
-                break;
-            }
-            printf("Received: %s\n", buffer);
+        // loop through the pollfd list
+        // note: index 0 is the listening socket
+        for (size_t i = 0; i < pollfds.size(); i++) {
+            // if it's the listening socket, check for incoming connections
+            if (i == 0 && (pollfds[i].revents & POLLIN)) {
+                // accept as many new connections as we can
+                while (true) {
+                    struct sockaddr_in clientAddress;
+                    socklen_t clientAddrSize = sizeof(clientAddress);
+                    int connfd = accept(socket_fd, (struct sockaddr *)&clientAddress, &clientAddrSize);
+                    if (connfd < 0) {
+                        // if no more connections are waiting, break out
+                        if (errno == EAGAIN || errno == EWOULDBLOCK)
+                            break;
+                        perror("accept");
+                        break;
+                    }
+                    printf("accepted a connection.\n");
 
-            // send respose to client.
-            const char* response = "Hello from server!\n";
-            ssize_t bytesSent = send(connfd, response, strlen(response), 0);
-            if (bytesSent < 0) {
-                perror("send()");
-                break;
-            }
-        }
-        // close current connection before server acceps another one
-        close(connfd);
-    }
+                    // set the new connection to non-blocking
+                    set_nonblocking(connfd);
 
-    //c lose the listening socket if loop breaks
+                    // add this connection to our pollfd vector, watch for read events
+                    struct pollfd client_pfd;
+                    client_pfd.fd = connfd;
+                    client_pfd.events = POLLIN;
+                    client_pfd.revents = 0;
+                    pollfds.push_back(client_pfd);
+                }
+            }
+            // handle client connections
+            else if (i > 0) {
+                // if there is any error or hangup, close the connection
+                if (pollfds[i].revents & (POLLERR | POLLHUP | POLLNVAL)) {
+                    printf("closing connection (error/hangup).\n");
+                    close(pollfds[i].fd);
+                    pollfds.erase(pollfds.begin() + i);
+                    i--; // adjust index after removal
+                    continue;
+                }
+                // if the socket is ready for reading
+                if (pollfds[i].revents & POLLIN) {
+                    char buffer[1024] = {0};
+                    ssize_t bytesRead = read(pollfds[i].fd, buffer, sizeof(buffer) - 1);
+                    if (bytesRead < 0) {
+                        // if nothing to read, continue
+                        if (errno == EAGAIN || errno == EWOULDBLOCK)
+                            continue;
+                        perror("read");
+                        close(pollfds[i].fd);
+                        pollfds.erase(pollfds.begin() + i);
+                        i--;
+                        continue;
+                    } else if (bytesRead == 0) {
+                        // connection closed by client
+                        printf("client disconnected.\n");
+                        close(pollfds[i].fd);
+                        pollfds.erase(pollfds.begin() + i);
+                        i--;
+                        continue;
+                    } else {
+                        buffer[bytesRead] = '\0';
+                        printf("received: %s\n", buffer);
+                        // send a response
+                        const char* response = "hello from server!\n";
+                        ssize_t bytesSent = send(pollfds[i].fd, response, strlen(response), 0);
+                        if (bytesSent < 0) {
+                            perror("send");
+                            close(pollfds[i].fd);
+                            pollfds.erase(pollfds.begin() + i);
+                            i--;
+                            continue;
+                        }
+                    }
+                }
+            }
+        } // end for loop over pollfds
+    } // end event loop
+
     close(socket_fd);
     return 0;
 }
