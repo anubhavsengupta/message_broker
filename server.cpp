f2757193dd06fc40bcf2264291ba77bc7eeb5f39
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2025-04-13 22:29:59 -0400

    Feat: Incorporate hashtable

--- server.cpp
+++ server.cpp
@@ -8,12 +8,13 @@
 #include <fcntl.h>       // for fcntl, O_NONBLOCK
 #include <poll.h>        // for poll
 #include <vector>
-#include <unordered_map>
 #include <string>
 #include <sstream>
 
-// simple key value store
-std::unordered_map<std::string, std::string> kv_store;
+#include "hashtable.h"
+
+// create a global hashtable for our key–value store, using 1024 buckets
+hashtable *ht = ht_create(1024);
 
 void die(const char* msg) {
     perror(msg);
@@ -30,31 +31,32 @@ static void set_nonblocking(int fd) {
         die("fcntl(set)");
 }
 
-// simple command parser: supports "SET key value" and "GET key"
+// simple command parser supporting "SET key value" and "GET key"
+// returns a std::string response
 std::string process_command(const std::string& cmd) {
     std::istringstream iss(cmd);
     std::string token;
     iss >> token;
-
-    if (token == "SET") {
+    if(token == "SET") {
         std::string key, value;
         iss >> key;
-        // get rest of line as value
         std::getline(iss, value);
         // trim leading spaces in value
         size_t start = value.find_first_not_of(" ");
-        if (start != std::string::npos)
+        if(start != std::string::npos)
             value = value.substr(start);
-        kv_store[key] = value;
-        return "OK\n";
-    } else if (token == "GET") {
+        if(ht_set(ht, key.c_str(), value.c_str()) == 0)
+            return "OK\n";
+        else
+            return "ERR setting value\n";
+    } else if(token == "GET") {
         std::string key;
         iss >> key;
-        if (kv_store.count(key)) {
-            return kv_store[key] + "\n";
-        } else {
+        char *val = ht_get(ht, key.c_str());
+        if(val)
+            return std::string(val) + "\n";
+        else
             return "(nil)\n";
-        }
     }
     return "ERR unknown command\n";
 }
@@ -65,13 +67,11 @@ int main() {
     if (socket_fd < 0) {
         die("socket()");
     }
-
-    // reuse port so we dont have to wait for the socket to free up
+    // reuse port so we don't have to wait for the socket to free up
     int opt = 1;
     if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         die("setsockopt()");
     }
-
     // setup server address
     struct sockaddr_in serverAddress;
     memset(&serverAddress, 0, sizeof(serverAddress));
@@ -83,7 +83,6 @@ int main() {
     if (bind(socket_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0) {
         die("bind()");
     }
-
     // start listening
     if (listen(socket_fd, 10) < 0) {
         die("listen()");
@@ -102,17 +101,15 @@ int main() {
     pfd.revents = 0;
     pollfds.push_back(pfd);
 
-    // event loop
+    // event loop: wait for readiness on our fds
     while (true) {
-        // wait for readiness on our fds (blocking call here)
         int rv = poll(pollfds.data(), pollfds.size(), -1);
         if (rv < 0) {
             if (errno == EINTR)
-                continue; // interrupted by a signal, retry
+                continue; // if interrupted by signal, retry
             die("poll()");
         }
-
-        // iterate over pollfds
+        // iterate over our pollfd list
         for (size_t i = 0; i < pollfds.size(); i++) {
             // if index 0 (listening socket) is ready, accept new connections
             if (i == 0 && (pollfds[i].revents & POLLIN)) {
@@ -122,12 +119,12 @@ int main() {
                     int connfd = accept(socket_fd, (struct sockaddr *)&clientAddress, &clientAddrSize);
                     if (connfd < 0) {
                         if (errno == EAGAIN || errno == EWOULDBLOCK)
-                            break; // no more incoming connections
+                            break; // no more pending connections
                         perror("accept");
                         break;
                     }
                     printf("accepted a connection.\n");
-                    // set new connection to non-blocking and add it to pollfds, watch for read events
+                    // set new connection to non-blocking and add to pollfds (watch for read events)
                     set_nonblocking(connfd);
                     struct pollfd client_pfd;
                     client_pfd.fd = connfd;
@@ -138,15 +135,15 @@ int main() {
             }
             // for client connections
             else if (i > 0) {
-                // if error/hangup, close and remove the connection
+                // if error/hangup, close and remove this connection
                 if (pollfds[i].revents & (POLLERR | POLLHUP | POLLNVAL)) {
                     printf("closing connection (error/hangup).\n");
                     close(pollfds[i].fd);
                     pollfds.erase(pollfds.begin() + i);
-                    i--;
+                    i--; // adjust index after removal
                     continue;
                 }
-                // if the socket is ready for reading
+                // if data available to read
                 if (pollfds[i].revents & POLLIN) {
                     char buffer[1024] = {0};
                     ssize_t bytesRead = read(pollfds[i].fd, buffer, sizeof(buffer) - 1);
@@ -159,7 +156,6 @@ int main() {
                         i--;
                         continue;
                     } else if (bytesRead == 0) {
-                        // connection closed by client
                         printf("client disconnected.\n");
                         close(pollfds[i].fd);
                         pollfds.erase(pollfds.begin() + i);
@@ -168,7 +164,7 @@ int main() {
                     } else {
                         buffer[bytesRead] = '\0';
                         printf("received: %s\n", buffer);
-                        // process command (note: for simplicity, we assume one command per read)
+                        // process command using our custom hashtable
                         std::string response = process_command(std::string(buffer));
                         ssize_t bytesSent = send(pollfds[i].fd, response.c_str(), response.length(), 0);
                         if (bytesSent < 0) {
@@ -181,9 +177,10 @@ int main() {
                     }
                 }
             }
-        } 
-    } 
+        } // end for over pollfds
+    } // end event loop
 
     close(socket_fd);
+    ht_destroy(ht); // free our hashtable
     return 0;
 }
